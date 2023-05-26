c5868fcc61cb51e80b6d107ba837fb7a9e581cc0
Enrico Piccini <e.piccini@crif.com>
2023-05-26 16:48:31 +0200

    fix indentation

--- src/lib/utils/computeRRule/fromString/computeYearlyInterval.js
+++ src/lib/utils/computeRRule/fromString/computeYearlyInterval.js
@@ -1,10 +1,10 @@
 const computeYearlyInterval = (data, rruleObj) => {
-    if (rruleObj.freq !== 1) {
-      return data.repeat.yearly.interval;
-    }
+  if (rruleObj.freq !== 1) {
+    return data.repeat.yearly.interval;
+  }
+
+  return rruleObj.interval;
+};
   
-    return rruleObj.interval;
-  };
-  
-  export default computeYearlyInterval;
+export default computeYearlyInterval;
   
